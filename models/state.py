6e38df334db685a731a1a6647fe748acea5673ef
rob64 <omar.best@gmail.com>
2024-04-01 16:05:07 -0600

    Improve engines
    updated FileStorage, DBStorage and State objects.

--- models/state.py
+++ models/state.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
 """This is the state class"""
-from sqlalchemy.ext.declarative import declarative_base
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
-import models
 from models.city import City
-import shlex
+from os import getenv
+
+HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "states"
+    """ State class """
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
+    cities = relationship("City", backref='state', cascade="all, delete, delete-orphan")
 
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+    if HBNB_TYPE_STORAGE != "db":
+        @property
+        def cities(self):
+            """Return a list with the cities"""
+            from models import storage
+            city_dict = storage.all(City)
+            city_list = []
+            for value in city_dict.values():
+                if value.state_id == self.id:
+                    city_list.append(value)
+            return city_list
